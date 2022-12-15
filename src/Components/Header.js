61d32cfa5810dd5f9aafe1f35411a1c342609eba
Bharathi-2109 <120362711+Bharathi-2109@users.noreply.github.com>
2022-12-15 11:06:05 +0530

    Initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+// importing material UI components
+import AppBar from "@mui/material/AppBar";
+
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+export default function Header() {
+  return <AppBar position="static"></AppBar>;
+}
